587c20f47006d0a49f6499966dde285fbfcde017
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2025-02-05 09:33:14 -0500

    Link routers

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-const bcrypt = require('bcryptjs');
-
-const jwt = require('jsonwebtoken');
-function verifyToken(req,res,next) {
-    const bearerHeader = req.headers.authorization;
-
-    if (bearerHeader !== undefined) {
-        const bearerToken = bearerHeader.split(' ').at(-1);
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
-
 const express = require('express');
+const {prisma} = require('./prismaClient.js');
+const ai = require('./aiCharacter.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const verifyToken = require('./verifyToken.js');
+
 const app = express();
-app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+const userRouter = require('./routes/users.js');
+const postRouter = require('./routes/posts.js');
+const likeRouter = require('./routes/like.js');
+const msgRouter = require('./routes/messages.js');
+
+app.use('/users',userRouter);
+app.use('/posts',postRouter);
+app.use('/likes',likeRouter);
+app.use('/messages',msgRouter);
 
 app.post('/log-in',async (req,res)=>{
     const user = await prisma.user.findFirst({
@@ -28,32 +26,17 @@ app.post('/log-in',async (req,res)=>{
             username: req.body.username
         }
     });
-    const match = await bcrypt.compare(req.user.password,user.password);
-
+    let match;
+    if (user.password) {
+        match = await bcrypt.compare(req.body.password,user.password);
+    }
     if (match) {
-        jwt.sign({user},process.env.SECRET_KEY,{expiresIn:'2h'},(err,token)=>{
+        jwt.sign({...user,password:undefined},process.env.SECRET_KEY,{expiresIn:'2h'},(err,token)=>{
             res.json({
                 token
             });
         });
     }
 });
-
-app.post('/users',async (req,res)=>{
-    const hashedPassword = await bcrypt.hash(req.body.password,10);
-    const user = await prisma.user.create({
-        data: {
-            username:req.body.username,
-            password:hashedPassword
-        }
-    });
-    res.json(user);
-});
-
-app.post('/posts');
-
-app.post('/characters');
-
-app.post('/messages')
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}!`));
