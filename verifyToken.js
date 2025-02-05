8ed47a0ff8c7c89c9c4c364ead7826a1c15fa405
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2025-02-05 09:34:43 -0500

    Edit middleware to send valid ids

--- verifyToken.js
+++ verifyToken.js
@@ -1,13 +1,35 @@
-function verifyToken(req,res,next) {
-    const bearerHeader = req.headers.authorization;
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const {prisma} = require('./prismaClient.js');
 
-    if (bearerHeader !== undefined) {
-        const bearerToken = bearerHeader.split(' ').at(-1);
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
+function verifyToken(req,res,next) {
+    const token = req.headers.authorization?.split(' ').at(-1);
+
+    if (!token) {
+        return res.status(401).json({message:"Access denied: No token provided"});
     }
+
+    jwt.verify(token,process.env.SECRET_KEY, async (err,authData) => {
+        if (err) {
+            return res.status(403).json({message:"Invalid token"});
+        }
+
+        try {
+            const characters = await prisma.aiProfile.findMany({where:{creatorId:authData.id}});
+            const validIds = characters.map(val=>val.userId);
+            validIds.push(authData.id);
+
+            req.validIds = validIds;
+
+            if (!req.validIds) {
+                return res.status(404).json({message:"User not found"});
+            }
+
+            next();
+        } catch (error) {
+            return res.status(500).json({message:"Internal Server Error", error});
+        }
+    });
 }
 
 module.exports = verifyToken;
